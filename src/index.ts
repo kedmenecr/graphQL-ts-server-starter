e01a21a2fd9f23cc04b6aac4742a6ee67d40cfb4
kedmenecr <kedmenec.roberto@gmail.com>
2018-08-10 21:12:44 +0200

    Add users/registration.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import "reflect-metadata";
-import { GraphQLServer } from 'graphql-yoga'
-import { importSchema } from 'graphql-import'
-import { resolvers } from './resolvers'
+import { importSchema } from "graphql-import";
+import { GraphQLServer } from "graphql-yoga";
+import { createConnection } from "typeorm";
+import * as path from "path";
 
-const typeDefs = importSchema("schema.graphql")
+import { resolvers } from "./resolvers";
 
+const typeDefs = importSchema(path.join(__dirname, "./schema.graphql"));
 
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+createConnection().then(() => {
+  server.start(() => console.log("Server is running on localhost:4000"));
+});
--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,10 +1,21 @@
-import { ResolverMap } from "./types/graphql-utils";
+import { ResolverMap } from "./types/graphql-utils"
+import * as bcrypt from 'bcryptjs'
+import { User } from "./entity/User";
+import { GQL } from "./types/schema";
 
 export const resolvers: ResolverMap = {
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Hello ${name || 'Lol'}`,
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => { }
+    register: async (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
+      const hashedPassword = await bcrypt.hash(password, 10)
+      const user = User.create({
+        email,
+        password: hashedPassword
+      });
+      await user.save();
+      return true;
+    }
   }
 };
