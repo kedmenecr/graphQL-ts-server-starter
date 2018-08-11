0f0848887f3648ccb0bd7270d2d760e6f77582dc
kedmenecr <kedmenec.roberto@gmail.com>
2018-08-11 16:08:45 +0200

    Add: Test database and enviromennts for dev and test.

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,8 +1,11 @@
 import { request } from "graphql-request";
 import { host } from "./constants";
-
-import { createConnection } from "typeorm";
 import { User } from "../entity/User";
+import { createTypeormConn } from "../utils/createTypeormConn";
+
+beforeAll(async () => {
+    await createTypeormConn();
+})
 
 const email = "abc@bobi.com"
 const password = "password123"
@@ -14,11 +17,11 @@ mutation{
 `
 
 test("Register user", async () => {
+
     const response = await request(host, mutation);
     expect(response).toEqual({ register: true });
-    await createConnection();
-    const users = await User.find({ where: { email } });
 
+    const users = await User.find({ where: { email } });
     expect(users).toHaveLength(1);
     const user = users[0];
     expect(user.email).toEqual(email);
